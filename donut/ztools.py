696f5b7e26390bb1430e886c4240b6b7781a234e
Tiago Ribeiro de Souza <tiago.astro@gmail.com>
2017-08-28 09:56:48 -0300

    Still working on the float point indexing issue.

--- donut/ztools.py
+++ donut/ztools.py
@@ -299,5 +299,5 @@ def shift(matrix,s1,s2):
                    axis=0)
 
 def rebin(a, shape):
-    sh = shape[0],a.shape[0]//shape[0],shape[1],a.shape[1]//shape[1]
+    sh = int(shape[0]),int(a.shape[0]//shape[0]),int(shape[1]),int(a.shape[1]//shape[1])
     return a.reshape(sh).mean(-1).mean(1)
